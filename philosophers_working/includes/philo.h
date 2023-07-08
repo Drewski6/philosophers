8c30724b0c146bb02032c2a51d0e5f65ae26c7cf
Drew Pentland <drewtpentland@gmail.com>
2023-07-08 22:41:28 +0200

    basics getting set up

---
+++ philosophers_working/includes/philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 16:57:19 by dpentlan          #+#    #+#             */
+/*   Updated: 2023/07/05 17:05:09 by dpentlan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+//	philo.c
+
+#endif
