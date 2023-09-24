48fd90dbf6bd93b34a8dfcedb486825f50258817
Drew Pentland <drewtpentland@gmail.com>
2023-09-24 15:18:10 +0200

    Creating a linked list to hold thread_ids
    Using a linked list to hold all the thread ids so that Freeing is
    easier. Leaks if ran with valgrind. Also maybe make a nice visualizer
    function to print all the threads and thread_ids

--- philosophers_working/includes/philo.h
+++ philosophers_working/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 16:57:19 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/07/12 14:51:39 by dpentlan         ###   ########.fr       */
+/*   Updated: 2023/09/24 14:56:37 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,17 @@ typedef struct s_args
 	int	num_tt_eat;
 }			t_args;
 
+typedef struct s_philo
+{
+	pthread_t	thread_id;
+}			t_philo;
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
 //	philo.c
 
 //	time_utils.c
@@ -41,6 +52,18 @@ int		arg_parse(int argc, char **argv, t_args *args);
 void	ft_bzero(void *str, size_t len);
 bool	ft_atoi_backcheck(char *s_num, int num);
 int		ft_atoi(const char *nptr);
+
+//	threads.c
+int		create_one_philo(t_list **philos);
+int		create_n_philos(t_args *args, t_list **philos);
+int		destroy_all_philos(t_list **philos);
+
+//	lists.c
+t_list	*ft_lstnew(void *content);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 
 //	debug_funcs.c		To remove at the end.
 int		print_args(t_args *args);
